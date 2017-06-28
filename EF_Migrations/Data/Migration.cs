afb1a4e6a806f9b407e5e67fc9c8a9ffe80445b2
Martin Casserly <m.casserly@gmail.com>
2017-06-28 19:39:15 +0200

    Created Migration Data class

---
+++ EF_Migrations/Data/Migration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace EF_Migrations.Data
+{
+    class Migration
+    {
+        public string ProjectPath { get; set; }
+        public string MigrationName { get; set; }
+        public ProcessStartInfo StartInfo { get; set; }
+    }
+}
