491920776bbedd98e7421c89f343731ce02774e3
Martin Casserly <m.casserly@gmail.com>
2017-06-29 12:25:41 +0200

    More functions and some events.

--- EF_Migrations/Form1.Designer.cs
+++ EF_Migrations/Form1.Designer.cs
@@ -28,12 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblProjectPath = new System.Windows.Forms.Label();
+            this.tbxProjectPath = new System.Windows.Forms.TextBox();
+            this.btnProjectPath = new System.Windows.Forms.Button();
+            this.lblMigrationName = new System.Windows.Forms.Label();
+            this.tboxMigrationName = new System.Windows.Forms.TextBox();
+            this.gboxOptions = new System.Windows.Forms.GroupBox();
+            this.chboxRestore = new System.Windows.Forms.CheckBox();
+            this.chboxUpdate = new System.Windows.Forms.CheckBox();
+            this.btnMainAction = new System.Windows.Forms.Button();
+            this.gboxOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblProjectPath
+            // 
+            this.lblProjectPath.AutoSize = true;
+            this.lblProjectPath.Location = new System.Drawing.Point(13, 13);
+            this.lblProjectPath.Name = "lblProjectPath";
+            this.lblProjectPath.Size = new System.Drawing.Size(65, 13);
+            this.lblProjectPath.TabIndex = 0;
+            this.lblProjectPath.Text = "Project Path";
+            // 
+            // tbxProjectPath
+            // 
+            this.tbxProjectPath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tbxProjectPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxProjectPath.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbxProjectPath.ForeColor = System.Drawing.Color.White;
+            this.tbxProjectPath.Location = new System.Drawing.Point(13, 30);
+            this.tbxProjectPath.Name = "tbxProjectPath";
+            this.tbxProjectPath.Size = new System.Drawing.Size(365, 21);
+            this.tbxProjectPath.TabIndex = 1;
+            this.tbxProjectPath.TextChanged += new System.EventHandler(this.tbxProjectPath_TextChanged);
+            // 
+            // btnProjectPath
+            // 
+            this.btnProjectPath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnProjectPath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnProjectPath.Location = new System.Drawing.Point(385, 30);
+            this.btnProjectPath.Name = "btnProjectPath";
+            this.btnProjectPath.Size = new System.Drawing.Size(75, 21);
+            this.btnProjectPath.TabIndex = 2;
+            this.btnProjectPath.Text = "Browse...";
+            this.btnProjectPath.UseVisualStyleBackColor = false;
+            this.btnProjectPath.Click += new System.EventHandler(this.btnProjectPath_Click);
+            // 
+            // lblMigrationName
+            // 
+            this.lblMigrationName.AutoSize = true;
+            this.lblMigrationName.Location = new System.Drawing.Point(13, 75);
+            this.lblMigrationName.Name = "lblMigrationName";
+            this.lblMigrationName.Size = new System.Drawing.Size(82, 13);
+            this.lblMigrationName.TabIndex = 3;
+            this.lblMigrationName.Text = "Migration Name";
+            // 
+            // tboxMigrationName
+            // 
+            this.tboxMigrationName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tboxMigrationName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tboxMigrationName.Enabled = false;
+            this.tboxMigrationName.ForeColor = System.Drawing.Color.White;
+            this.tboxMigrationName.Location = new System.Drawing.Point(13, 92);
+            this.tboxMigrationName.Name = "tboxMigrationName";
+            this.tboxMigrationName.Size = new System.Drawing.Size(365, 21);
+            this.tboxMigrationName.TabIndex = 4;
+            this.tboxMigrationName.TextChanged += new System.EventHandler(this.tboxMigrationName_TextChanged);
+            // 
+            // gboxOptions
+            // 
+            this.gboxOptions.Controls.Add(this.chboxUpdate);
+            this.gboxOptions.Controls.Add(this.chboxRestore);
+            this.gboxOptions.ForeColor = System.Drawing.Color.White;
+            this.gboxOptions.Location = new System.Drawing.Point(13, 120);
+            this.gboxOptions.Name = "gboxOptions";
+            this.gboxOptions.Size = new System.Drawing.Size(447, 50);
+            this.gboxOptions.TabIndex = 5;
+            this.gboxOptions.TabStop = false;
+            this.gboxOptions.Text = "Options";
+            // 
+            // chboxRestore
+            // 
+            this.chboxRestore.AutoSize = true;
+            this.chboxRestore.Enabled = false;
+            this.chboxRestore.Location = new System.Drawing.Point(7, 21);
+            this.chboxRestore.Name = "chboxRestore";
+            this.chboxRestore.Size = new System.Drawing.Size(132, 17);
+            this.chboxRestore.TabIndex = 0;
+            this.chboxRestore.Text = "Restore dependencies";
+            this.chboxRestore.UseVisualStyleBackColor = true;
+            this.chboxRestore.CheckedChanged += new System.EventHandler(this.chboxRestore_CheckedChanged);
+            // 
+            // chboxUpdate
+            // 
+            this.chboxUpdate.AutoSize = true;
+            this.chboxUpdate.Enabled = false;
+            this.chboxUpdate.Location = new System.Drawing.Point(164, 21);
+            this.chboxUpdate.Name = "chboxUpdate";
+            this.chboxUpdate.Size = new System.Drawing.Size(109, 17);
+            this.chboxUpdate.TabIndex = 1;
+            this.chboxUpdate.Text = "Update Database";
+            this.chboxUpdate.UseVisualStyleBackColor = true;
+            this.chboxUpdate.CheckedChanged += new System.EventHandler(this.chboxUpdate_CheckedChanged);
+            // 
+            // btnMainAction
+            // 
+            this.btnMainAction.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnMainAction.Enabled = false;
+            this.btnMainAction.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnMainAction.Location = new System.Drawing.Point(12, 176);
+            this.btnMainAction.Name = "btnMainAction";
+            this.btnMainAction.Size = new System.Drawing.Size(448, 23);
+            this.btnMainAction.TabIndex = 6;
+            this.btnMainAction.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(472, 584);
+            this.Controls.Add(this.btnMainAction);
+            this.Controls.Add(this.gboxOptions);
+            this.Controls.Add(this.tboxMigrationName);
+            this.Controls.Add(this.lblMigrationName);
+            this.Controls.Add(this.btnProjectPath);
+            this.Controls.Add(this.tbxProjectPath);
+            this.Controls.Add(this.lblProjectPath);
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = ".NET Entity Framework Migrations";
+            this.gboxOptions.ResumeLayout(false);
+            this.gboxOptions.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblProjectPath;
+        private System.Windows.Forms.TextBox tbxProjectPath;
+        private System.Windows.Forms.Button btnProjectPath;
+        private System.Windows.Forms.Label lblMigrationName;
+        private System.Windows.Forms.TextBox tboxMigrationName;
+        private System.Windows.Forms.GroupBox gboxOptions;
+        private System.Windows.Forms.CheckBox chboxUpdate;
+        private System.Windows.Forms.CheckBox chboxRestore;
+        private System.Windows.Forms.Button btnMainAction;
     }
 }
 
--- EF_Migrations/Form1.cs
+++ EF_Migrations/Form1.cs
@@ -24,6 +24,7 @@ namespace EF_Migrations
 
         public void doRestore()
         {
+            Functions f = new Functions();
             Migration m = new Migration()
             {
 
@@ -32,6 +33,7 @@ namespace EF_Migrations
 
         public void doMigration()
         {
+            Functions f = new Functions();
             Migration m = new Migration()
             {
 
@@ -40,10 +42,39 @@ namespace EF_Migrations
 
         public void doUpdate()
         {
+            Functions f = new Functions();
             Migration m = new Migration()
             {
 
             };
+        }
+
+        public void clearAll()
+        {
+            tbxProjectPath.Text = "";
+            tboxMigrationName.Text = "";
+            chboxRestore.Checked = false;
+            chboxUpdate.Checked = false;
+        }
+
+        public void checkButtonStatus()
+        {
+            if (tbxProjectPath.Text == "")
+            {
+                btnMainAction.Enabled = false;
+                btnMainAction.Text = "";
+            }
+            else if (tbxProjectPath.Text != "")
+            {
+                if (tboxMigrationName.Text == "")
+                {
+                    if (!chboxRestore.Checked && !chboxUpdate.Checked)
+                    {
+                        btnMainAction.Enabled = false;
+                        btnMainAction.Text = "";
+                    }
+                }
+            }
         }
 
         #endregion FUNCTIONS
@@ -55,6 +86,50 @@ namespace EF_Migrations
             Console.Write(e.Data);
         }
 
+        private void tbxProjectPath_TextChanged(object sender, EventArgs e)
+        {
+            if (tbxProjectPath.Text == "")
+            {
+                tboxMigrationName.Enabled = false;
+                chboxRestore.Enabled = false;
+                chboxUpdate.Enabled = false;
+            }
+            else if (tbxProjectPath.Text != "")
+            {
+                tboxMigrationName.Enabled = true;
+                chboxRestore.Enabled = true;
+                chboxUpdate.Enabled = true;
+            }
+        }
+
+        private void tboxMigrationName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chboxRestore_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chboxUpdate_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void btnProjectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Select project folder";
+            fbd.ShowNewFolderButton = false;
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbxProjectPath.Text = fbd.SelectedPath;
+            }
+        }
+
         #endregion EVENTS
+
     }
 }
