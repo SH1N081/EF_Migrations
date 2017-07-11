2c81161809bea079a8f7715a722c8f636e6b4fbf
Martin Casserly <m.casserly@gmail.com>
2017-07-11 10:42:23 +0200

    updated Migration class with createProcessList()

--- EF_Migrations/Data/Migration.cs
+++ EF_Migrations/Data/Migration.cs
@@ -13,7 +13,68 @@ namespace EF_Migrations.Data
         public string MigrationName { get; set; }
         public int Action { get; set; }
 
-        private string _command;
+        private List<string> _commands
+        {
+            get
+            {
+                List<string> commands = new List<string>();
+
+                switch (Action)
+                {
+                    case 0:
+                        commands.Clear();
+                        return commands;
+                    case 1:
+                        commands.Clear();
+                        string r = "dotnet restore";
+                        commands.Add(r);
+                        return commands;
+                    case 2:
+                        commands.Clear();
+                        string m = "dotnet ef migrations add " + MigrationName;
+                        commands.Add(m);
+                        return commands;
+                    case 3:
+                        commands.Clear();
+                        string rm1 = "dotnet restore";
+                        string rm2 = "dotnet ef migrations add " + MigrationName;
+                        commands.Add(rm1);
+                        commands.Add(rm2);
+                        return commands;
+                    case 4:
+                        commands.Clear();
+                        string ru1 = "dotnet restore";
+                        string ru2 = "dotnet ef database update";
+                        commands.Add(ru1);
+                        commands.Add(ru2);
+                        return commands;
+                    case 5:
+                        commands.Clear();
+                        string rmu1 = "dotnet restore";
+                        string rmu2 = "dotnet ef migrations add " + MigrationName;
+                        string rmu3 = "dotnet ef database update";
+                        commands.Add(rmu1);
+                        commands.Add(rmu2);
+                        commands.Add(rmu3);
+                        return commands;
+                    case 6:
+                        commands.Clear();
+                        string mu1 = "dotnet ef migrations add " + MigrationName;
+                        string mu2 = "dotnet ef database update";
+                        commands.Add(mu1);
+                        commands.Add(mu2);
+                        return commands;
+                    case 7:
+                        commands.Clear();
+                        string u = "dotnet ef database update";
+                        commands.Add(u);
+                        return commands;
+                    default:
+                        commands.Clear();
+                        return commands;
+                }
+            }
+        }
 
         public enum actions
         {
@@ -29,14 +90,25 @@ namespace EF_Migrations.Data
 
         //Add definitions for the different commands
 
-        public Process createProcess()
+        public List<Process> createProcessList()
         {
-            Process p = new Process();
-            p.StartInfo = new ProcessStartInfo()
+            List<string> commands = _commands;
+            List<Process> pList = new List<Process>();
+            foreach (string c in commands)
             {
-
-            };
-            return p;
+                Process p = new Process();
+                p.StartInfo = new ProcessStartInfo()
+                {
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardError = true,
+                    FileName = "cmd.exe",
+                    WorkingDirectory = ProjectPath,
+                    Arguments = c
+                };
+                pList.Add(p);
+            }
+            return pList;
         }
     }
 }
